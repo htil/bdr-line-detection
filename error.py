41af73499b308e37f7d760a3907b57f1167021e2
Elijah Sawyers <elijahsawyers@gmail.com>
2018-09-20 15:15:53 -0500

    Red edge detection

--- error.py
+++ error.py
@@ -3,20 +3,26 @@ import numpy as np
 
 if __name__ == "__main__":
 	img = cv2.imread('test.jpg')
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	edges = cv2.Canny(gray,50,150,apertureSize = 3)
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	lines = cv2.HoughLines(edges,1,np.pi/180,500)
+	lower_range = np.array([0, 100, 100], dtype=np.uint8)
+	upper_range = np.array([15, 255, 255], dtype=np.uint8)
+
+	mask = cv2.inRange(hsv, lower_range, upper_range)
+
+	edges = cv2.Canny(mask,50,150,apertureSize = 3)
+
+	lines = cv2.HoughLines(edges,1,np.pi/180,1000)
 	for line in lines:
 	    a = np.cos(line[0][1])
 	    b = np.sin(line[0][1])
 	    x0 = a*line[0][0]
 	    y0 = b*line[0][0]
-	    x1 = int(x0 + 1000*(-b))
-	    y1 = int(y0 + 1000*(a))
-	    x2 = int(x0 - 1000*(-b))
-	    y2 = int(y0 - 1000*(a))
-
-	    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+	    x1 = int(x0 + 100000*(-b))
+	    y1 = int(y0 + 100000*(a))
+	    x2 = int(x0 - 100000*(-b))
+	    y2 = int(y0 - 100000*(a))
+	    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
 	cv2.imwrite('result.jpg',img)
+
